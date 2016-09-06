2cbeb7c24912b8e043c9982ddd1560286964773f
Soumitra Goswami <sgoswami@broadsoft.com>
2016-09-06 12:19:51 +0530

    First commit

---
+++ src/main/java/com/Broadsoft/controller/Controller.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Broadsoft.controller;
+
+import com.Broadsoft.model.StudentModel;
+import com.Broadsoft.repository.StudentRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author udey
+ */
+    @RestController
+
+public class Controller {
+        
+       @Autowired
+       StudentRepository su;
+        
+    @RequestMapping(value = "/find/{id}", method = RequestMethod.GET)
+    public List<StudentModel> greeting(@PathVariable("id") String id) {
+        
+       return su.getUsers(id);
+    }
+
+    @RequestMapping("/")
+    public List<StudentModel> WelcomeMessage() {
+         return  su.getUser();
+    }
+}
+
